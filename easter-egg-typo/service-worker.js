678d37463fff26d65448e2f253440c3e82287d43
porisius <47161774+porisius@users.noreply.github.com>
2023-06-12 01:42:00 +0000

    Deploying to gh-pages from @ porisius/smr-frontend@764e5e57f469f1d5959f71549c4b2ec0f21ecdde 🚀

---
+++ easter-egg-typo/service-worker.js
@@ -0,0 +1,55 @@
+const e = { toString: () => {
+  throw new Error("`timestamp` has been removed from $service-worker. Use `version` instead");
+} };
+const s = ["/smr-frontend/easter-egg-typo/_app/immutable/start-b2fde949.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/__layout.svelte-6ac0d73d.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/pages/__layout.svelte-3cd13dee.css", "/smr-frontend/easter-egg-typo/_app/immutable/error.svelte-465a1520.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/admin/index.svelte-b0b36299.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/pages/admin/index.svelte-dfc5337c.css", "/smr-frontend/easter-egg-typo/_app/immutable/pages/admin/sml-versions/_smlVersionId_/edit.svelte-9167587f.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/admin/sml-versions/index.svelte-146bf3b5.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/admin/sml-versions/new.svelte-730faa17.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/admin/tag-manager.svelte-2f9450f6.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/admin/unapproved-mods.svelte-166fc476.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/admin/unapproved-versions.svelte-99561626.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/api-docs/index.svelte-aa106d18.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/pages/privacy-policy/index.svelte-bcc48566.css", "/smr-frontend/easter-egg-typo/_app/immutable/pages/guide/_guideId_/edit.svelte-ef316428.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/guide/_guideId_/index.svelte-e3bffcc2.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/guides/index.svelte-1e0c2bf3.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/help/index.svelte-4751c28d.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/index.svelte-53efc363.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/pages/index.svelte-fe20af75.css", "/smr-frontend/easter-egg-typo/_app/immutable/pages/mod/_modId_/edit.svelte-42e8bf69.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/mod/_modId_/index.svelte-36b15999.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/pages/mod/_modId_/index.svelte-1e2ec0d7.css", "/smr-frontend/easter-egg-typo/_app/immutable/pages/mod/_modId_/new-version.svelte-6cc5e5ac.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/mod/_modId_/version/_versionId_/edit.svelte-69e143be.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/mod/_modId_/version/_versionId_/index.svelte-ea54d434.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/mod/_modId_/version/_versionId_/view.svelte-bbfd40b2.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/mods/index.svelte-32d64272.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/pages/mods/index.svelte-07b34bd3.css", "/smr-frontend/easter-egg-typo/_app/immutable/pages/new-guide/index.svelte-5062a5bd.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/new-mod/index.svelte-8ee696f0.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/privacy-policy/index.svelte-d1834f4d.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/settings/index.svelte-52da8474.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/sml-versions/index.svelte-24239154.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/tools/index.svelte-52d06fd3.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/tos/index.svelte-653d165d.js", "/smr-frontend/easter-egg-typo/_app/immutable/pages/user/_userId_/index.svelte-80ef5ebf.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/extras-8984d02f.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/forms-e7a1b133.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/jszip-0721729d.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/vendor-22698a15.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/paths-1c47712a.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/singletons-cdeec3fd.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/preload-helper-901ad276.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/global-f30eeeff.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/user-6e2eeec5.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/navigation-ce539261.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/launcher-c348dc95.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/api-bb6d5972.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/graphql-43a8ad25.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/stores-a04bccbe.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/Toast-3f77241b.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/MetaDescriptors-fd872502.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/routing-c6322149.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/SMLVersionForm-7f2b67e3.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/ModForm-cc1db5c2.css", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/forms-874c16ac.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/markdown-6a2ff7f0.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/PageControls-ca9de549.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/formatting-9fd7b49b.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/GuideForm-2c14e279.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/gql-a60281d5.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/GuideCard-ec184009.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/FicsitCard-fe347dff.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/FicsitCard-4de49b41.css", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/uplugin-74e51756.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/ModCard-5ec95183.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/CompatibilityButton-23eab7b7.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/TagList-45681d2c.js", "/smr-frontend/easter-egg-typo/_app/immutable/assets/TagList-92a9d7da.css", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/ModForm-0c876215.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/ModCompatibilityEdit-051ea801.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/VersionForm-2c867161.js", "/smr-frontend/easter-egg-typo/_app/immutable/chunks/custom_protocol-9a0f337a.js"];
+const t = ["/smr-frontend/easter-egg-typo/assets/android-chrome-192x192.png", "/smr-frontend/easter-egg-typo/assets/android-chrome-512x512.png", "/smr-frontend/easter-egg-typo/assets/apple-touch-icon.png", "/smr-frontend/easter-egg-typo/assets/browserconfig.xml", "/smr-frontend/easter-egg-typo/assets/favicon-16x16.png", "/smr-frontend/easter-egg-typo/assets/favicon-32x32.png", "/smr-frontend/easter-egg-typo/assets/favicon.ico", "/smr-frontend/easter-egg-typo/assets/fonts/flow-rounded.woff", "/smr-frontend/easter-egg-typo/assets/mstile-150x150.png", "/smr-frontend/easter-egg-typo/assets/safari-pinned-tab.svg", "/smr-frontend/easter-egg-typo/images/Satisfactory_KeyArt_1_1024x1024_NoLogo.webp", "/smr-frontend/easter-egg-typo/images/bg_nologo.webp", "/smr-frontend/easter-egg-typo/images/dog_aw_yis_pats.gif", "/smr-frontend/easter-egg-typo/images/dog_boing.gif", "/smr-frontend/easter-egg-typo/images/dog_stand.gif", "/smr-frontend/easter-egg-typo/images/mods_mobile.webp", "/smr-frontend/easter-egg-typo/images/no_image.webp", "/smr-frontend/easter-egg-typo/images/satisfactory_logo_full_color_small.webp", "/smr-frontend/easter-egg-typo/images/sf_modding_logo.webp", "/smr-frontend/easter-egg-typo/images/smm_background.webp", "/smr-frontend/easter-egg-typo/images/smm_hand.webp", "/smr-frontend/easter-egg-typo/images/smm_icon_white.webp", "/smr-frontend/easter-egg-typo/manifest.json", "/smr-frontend/easter-egg-typo/robots.txt", "/smr-frontend/easter-egg-typo/smui.css"];
+const a = self;
+const r = `cache${e}`;
+const n = s.concat(t);
+const o = new Set(n);
+a.addEventListener("install", (e2) => {
+  e2.waitUntil(caches.open(r).then((e3) => e3.addAll(n)).then(() => {
+    a.skipWaiting();
+  }));
+});
+a.addEventListener("activate", (e2) => {
+  e2.waitUntil(caches.keys().then(async (e3) => {
+    for (const s2 of e3) {
+      if (s2 !== r) {
+        await caches.delete(s2);
+      }
+    }
+    a.clients.claim();
+  }));
+});
+async function p(s2) {
+  const t2 = await caches.open(`offline${e}`);
+  try {
+    const e2 = await fetch(s2);
+    t2.put(s2, e2.clone());
+    return e2;
+  } catch (e2) {
+    const a2 = await t2.match(s2);
+    if (a2) {
+      return a2;
+    }
+    throw e2;
+  }
+}
+a.addEventListener("fetch", (e2) => {
+  if (e2.request.method !== "GET" || e2.request.headers.has("range")) {
+    return;
+  }
+  const s2 = new URL(e2.request.url);
+  const t2 = s2.protocol.startsWith("http");
+  const a2 = s2.hostname === self.location.hostname && s2.port !== self.location.port;
+  const r2 = s2.host === self.location.host && o.has(s2.pathname);
+  const n2 = e2.request.cache === "only-if-cached" && !r2;
+  if (t2 && !a2 && !n2 && r2 && !e2.request.url.includes("start")) {
+    e2.respondWith((async () => {
+      const s3 = r2 && await caches.match(e2.request);
+      return s3 || p(e2.request);
+    })());
+  }
+});
+//# sourceMappingURL=service-worker.js.map
