041ce773c07658dd2cb89617e36cb4f1a55d8615
Vilsol <me@vil.so>
2023-10-19 20:57:12 +0000

    Deploying to gh-pages from @ porisius/smr-frontend@a25cc1595e6b1b966c5e89b771e78cb7a3bcc667 🚀

---
+++ dedi-releases/service-worker.js
@@ -0,0 +1,53 @@
+const e = ["/smr-frontend/dedi-releases/_app/immutable/start-c4b6e90b.js", "/smr-frontend/dedi-releases/_app/immutable/pages/__layout.svelte-75e3310b.js", "/smr-frontend/dedi-releases/_app/immutable/assets/pages/__layout.svelte-7bcd9762.css", "/smr-frontend/dedi-releases/_app/immutable/error.svelte-d35a6d97.js", "/smr-frontend/dedi-releases/_app/immutable/pages/admin/index.svelte-aae00f17.js", "/smr-frontend/dedi-releases/_app/immutable/assets/pages/admin/index.svelte-dfc5337c.css", "/smr-frontend/dedi-releases/_app/immutable/pages/admin/sml-versions/_smlVersionId_/edit.svelte-2c7fe7d9.js", "/smr-frontend/dedi-releases/_app/immutable/pages/admin/sml-versions/index.svelte-1d8c8a39.js", "/smr-frontend/dedi-releases/_app/immutable/pages/admin/sml-versions/new.svelte-f5246839.js", "/smr-frontend/dedi-releases/_app/immutable/pages/admin/tag-manager.svelte-f5ce023e.js", "/smr-frontend/dedi-releases/_app/immutable/pages/admin/unapproved-mods.svelte-b0df404c.js", "/smr-frontend/dedi-releases/_app/immutable/pages/admin/unapproved-versions.svelte-63d59105.js", "/smr-frontend/dedi-releases/_app/immutable/pages/api-docs/index.svelte-f66b645a.js", "/smr-frontend/dedi-releases/_app/immutable/assets/pages/api-docs/index.svelte-1c1e4dfd.css", "/smr-frontend/dedi-releases/_app/immutable/pages/guide/_guideId_/edit.svelte-51d44a7f.js", "/smr-frontend/dedi-releases/_app/immutable/pages/guide/_guideId_/index.svelte-3a4b2e60.js", "/smr-frontend/dedi-releases/_app/immutable/pages/guides/index.svelte-ddc43825.js", "/smr-frontend/dedi-releases/_app/immutable/pages/help/index.svelte-ecb63ef4.js", "/smr-frontend/dedi-releases/_app/immutable/pages/index.svelte-99bd9b51.js", "/smr-frontend/dedi-releases/_app/immutable/assets/pages/index.svelte-fe20af75.css", "/smr-frontend/dedi-releases/_app/immutable/pages/mod/_modId_/edit.svelte-a7c6b70f.js", "/smr-frontend/dedi-releases/_app/immutable/pages/mod/_modId_/index.svelte-b2c373d3.js", "/smr-frontend/dedi-releases/_app/immutable/assets/pages/mod/_modId_/index.svelte-1e2ec0d7.css", "/smr-frontend/dedi-releases/_app/immutable/pages/mod/_modId_/new-version.svelte-0058a6b3.js", "/smr-frontend/dedi-releases/_app/immutable/pages/mod/_modId_/version/_versionId_/edit.svelte-c5fa4266.js", "/smr-frontend/dedi-releases/_app/immutable/pages/mod/_modId_/version/_versionId_/index.svelte-93963109.js", "/smr-frontend/dedi-releases/_app/immutable/pages/mod/_modId_/version/_versionId_/view.svelte-9faedf57.js", "/smr-frontend/dedi-releases/_app/immutable/pages/mods/index.svelte-608650bb.js", "/smr-frontend/dedi-releases/_app/immutable/assets/pages/mods/index.svelte-07b34bd3.css", "/smr-frontend/dedi-releases/_app/immutable/pages/new-guide/index.svelte-ae1ac999.js", "/smr-frontend/dedi-releases/_app/immutable/pages/new-mod/index.svelte-da678fca.js", "/smr-frontend/dedi-releases/_app/immutable/pages/privacy-policy/index.svelte-3c0eab11.js", "/smr-frontend/dedi-releases/_app/immutable/pages/settings/index.svelte-b6a4b5e2.js", "/smr-frontend/dedi-releases/_app/immutable/pages/sml-versions/index.svelte-02038e31.js", "/smr-frontend/dedi-releases/_app/immutable/pages/tools/index.svelte-d168aa86.js", "/smr-frontend/dedi-releases/_app/immutable/pages/tos/index.svelte-9c3dab43.js", "/smr-frontend/dedi-releases/_app/immutable/pages/user/_userId_/index.svelte-2b0baf4b.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/extras-8984d02f.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/forms-76acb165.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/jszip-0721729d.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/vendor-5faff0be.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/paths-1c47712a.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/singletons-cdeec3fd.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/preload-helper-ebc9e0c5.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/global-fb89089b.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/user-cee14147.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/navigation-ce539261.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/launcher-aafc45b7.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/api-bb6d5972.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/graphql-1bd1fc10.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/stores-a6f790a9.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/Toast-13eea52f.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/MetaDescriptors-5e0585a5.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/routing-c6322149.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/SMLVersionForm-a4f5d314.js", "/smr-frontend/dedi-releases/_app/immutable/assets/SMLVersionForm-1cc6d1a4.css", "/smr-frontend/dedi-releases/_app/immutable/chunks/forms-1d9b5922.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/markdown-6a2ff7f0.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/PageControls-3bf45888.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/formatting-feef42d8.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/GuideForm-c9ff201e.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/gql-24499839.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/GuideCard-bca60a14.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/FicsitCard-ea66121f.js", "/smr-frontend/dedi-releases/_app/immutable/assets/FicsitCard-4de49b41.css", "/smr-frontend/dedi-releases/_app/immutable/chunks/uplugin-ba5939c1.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/ModCard-19ecabd2.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/CompatibilityIcon-9f13adcb.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/TagList-3ea5ac95.js", "/smr-frontend/dedi-releases/_app/immutable/assets/TagList-92a9d7da.css", "/smr-frontend/dedi-releases/_app/immutable/chunks/ModForm-8046de89.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/ModCompatibilityEdit-b42ca677.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/VersionDependenciesGrid-9db321c5.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/VersionForm-2be82292.js", "/smr-frontend/dedi-releases/_app/immutable/chunks/custom_protocol-0b6ab32e.js"];
+const s = ["/smr-frontend/dedi-releases/assets/android-chrome-192x192.png", "/smr-frontend/dedi-releases/assets/android-chrome-512x512.png", "/smr-frontend/dedi-releases/assets/apple-touch-icon.png", "/smr-frontend/dedi-releases/assets/browserconfig.xml", "/smr-frontend/dedi-releases/assets/favicon-16x16.png", "/smr-frontend/dedi-releases/assets/favicon-32x32.png", "/smr-frontend/dedi-releases/assets/favicon.ico", "/smr-frontend/dedi-releases/assets/fonts/flow-rounded.woff", "/smr-frontend/dedi-releases/assets/mstile-150x150.png", "/smr-frontend/dedi-releases/assets/safari-pinned-tab.svg", "/smr-frontend/dedi-releases/images/Satisfactory_KeyArt_1_1024x1024_NoLogo.webp", "/smr-frontend/dedi-releases/images/bg_nologo.webp", "/smr-frontend/dedi-releases/images/dog_aw_yis_pats.gif", "/smr-frontend/dedi-releases/images/dog_boing.gif", "/smr-frontend/dedi-releases/images/dog_stand.gif", "/smr-frontend/dedi-releases/images/mods_mobile.webp", "/smr-frontend/dedi-releases/images/no_image.webp", "/smr-frontend/dedi-releases/images/satisfactory_logo_full_color_small.webp", "/smr-frontend/dedi-releases/images/sf_modding_logo.webp", "/smr-frontend/dedi-releases/images/smm_background.webp", "/smr-frontend/dedi-releases/images/smm_hand.webp", "/smr-frontend/dedi-releases/images/smm_icon_white.webp", "/smr-frontend/dedi-releases/manifest.json", "/smr-frontend/dedi-releases/robots.txt", "/smr-frontend/dedi-releases/smui.css"];
+const a = "1697748988212";
+const d = self;
+const r = `cache${a}`;
+const n = e.concat(s);
+const t = new Set(n);
+d.addEventListener("install", (e2) => {
+  e2.waitUntil(caches.open(r).then((e3) => e3.addAll(n)).then(() => {
+    d.skipWaiting();
+  }));
+});
+d.addEventListener("activate", (e2) => {
+  e2.waitUntil(caches.keys().then(async (e3) => {
+    for (const s2 of e3) {
+      if (s2 !== r) {
+        await caches.delete(s2);
+      }
+    }
+    d.clients.claim();
+  }));
+});
+async function i(e2) {
+  const s2 = await caches.open(`offline${a}`);
+  try {
+    const a2 = await fetch(e2);
+    s2.put(e2, a2.clone());
+    return a2;
+  } catch (a2) {
+    const d2 = await s2.match(e2);
+    if (d2) {
+      return d2;
+    }
+    throw a2;
+  }
+}
+d.addEventListener("fetch", (e2) => {
+  if (e2.request.method !== "GET" || e2.request.headers.has("range")) {
+    return;
+  }
+  const s2 = new URL(e2.request.url);
+  const a2 = s2.protocol.startsWith("http");
+  const d2 = s2.hostname === self.location.hostname && s2.port !== self.location.port;
+  const r2 = s2.host === self.location.host && t.has(s2.pathname);
+  const n2 = e2.request.cache === "only-if-cached" && !r2;
+  if (a2 && !d2 && !n2 && r2 && !e2.request.url.includes("start")) {
+    e2.respondWith((async () => {
+      const s3 = r2 && await caches.match(e2.request);
+      return s3 || i(e2.request);
+    })());
+  }
+});
+//# sourceMappingURL=service-worker.js.map
